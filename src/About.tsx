81d8f53b6b195c8a2a4fce214a22d91ef2395070
MassanovSalimkhan <massanov_s@iuca.kg>
2024-11-21 19:45:46 +0600

    Initial commit

---
+++ src/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This is a simple React app built with Vite and TypeScript.</p>
+    </div>
+  );
+};
+
+export default About;
